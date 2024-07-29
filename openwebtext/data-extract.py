098be314bab885ca3a17f2a23a4a0cbb0b0cfa88
shakyanjil <shakya.anjil01@gmail.com>
2024-07-29 17:08:44 +0900

    data-extraction script is added

---
+++ openwebtext/data-extract.py
@@ -0,0 +1,62 @@
+import os
+import lzma
+from tqdm import tqdm
+
+def xz_files_in_dir(directory):
+    files = []
+    for filename in os.listdir(directory):
+        if filename.endswith(".xz") and os.path.isfile(os.path.join(directory, filename)):
+            files.append(filename)
+    return files
+
+folder_path = "C:/Users/Anjil/Documents/LearningDataScience/openwebtext/"
+output_file_train = "output_train.txt"
+output_file_val = "output_val.txt"
+vocab_file = "vocab.txt"
+
+files = xz_files_in_dir(folder_path)
+total_files = len(files)
+
+split_index = int(total_files * 0.9)
+files_train = files[:split_index]
+files_val = files[split_index:]
+
+vocab = set()
+
+# Proces the training files
+with open(output_file_train, "w", encoding="utf-8") as outfile:
+    for filename in tqdm(files_train, total=len(files_train)):
+        file_path = os.path.join(folder_path, filename)
+        with lzma.open(file_path, "rt", encoding="utf-8") as infile:
+            text=infile.read()
+            outfile.write(text)
+            characters = set(text)
+            vocab.update(characters)
+
+# Process the validation files
+with open(output_file_val, "w", encoding="utf-8") as outfile:
+    for filename in tqdm(files_val, total=len(files_val)):
+        file_path = os.path.join(folder_path, filename)
+        with lzma.open(file_path, "rt", encoding="utf-8") as infile:
+            text=infile.read()
+            outfile.write(text)
+            characters = set(text)
+            vocab.update(characters)
+
+
+# for i in range(split_files):
+#     with open(output_file.format(i), "w", encoding="utf-8") as outfile:
+#         for count, filename in enumerate(tqdm(files[:max_count], total=max_count)):
+#             if count >= max_count:
+#                 break
+#             file_path = os.path.join(folder_path, filename)
+#             with lzma.open(file_path, "rt", encoding="utf-8") as infile:
+#                 text = infile.read()
+#                 outfile.write(text)
+#                 characters = set(text)
+#                 vocab.update(characters)
+#         files = files[max_count:]
+
+with open(vocab_file, "w", encoding="utf-8") as vfile:
+    for char in vocab:
+        vfile.write(char + '\n')
